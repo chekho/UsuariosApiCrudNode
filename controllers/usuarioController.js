ae5d53dca20ff342c62329303c30416a5c8b0cd0
IDGS-801-19002051 <156255910+IDGS-801-19002051@users.noreply.github.com>
2024-10-31 08:04:08 -0600

    Implementa PUT de Usuarios
    -Se ha implementado el PUT de Usuarios para la modificación de los mismos
    -Se ha documentado en swagger

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -80,7 +80,46 @@ const crearUsuario = (req, res) => {
   });
 };
 
-const actualizarUsuario = (req, res) => {};
+const actualizarUsuario = (req, res) => {
+  let id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    res.status(400).json({ err: true, message: "Id invalido" });
+    return;
+  }
+  let usuarioIndex = usuarios.findIndex((usuario) => usuario.id === id);
+  if (usuarioIndex === -1) {
+    res.status(404).json({ err: true, message: "Usuario no encontrado" });
+    return;
+  }
+  let nombre = req.body.nombre;
+  let apellido = req.body.apellido || "";
+  let email = req.body.email;
+  if (!nombre && !email) {
+    res.status(400).json({ err: true, message: "Nombre y email requeridos" });
+    return;
+  }
+  if (!email.includes("@") || !email.includes(".")) {
+    res.status(400).json({ err: true, message: "Formato de email invalido" });
+    return;
+  }
+  if (usuarios.find((usuario) => usuario.email === email)) {
+    res.status(400).json({ err: true, message: "Email ya existe" });
+    return;
+  }
+  if (nombre.match(/\d+/) || apellido.match(/\d+/)) {
+    res.status(400).json({
+      err: true,
+      message: "Nombre y apellido no pueden tener numeros",
+    });
+    return;
+  }
+  usuarios[usuarioIndex].nombre = nombre;
+  usuarios[usuarioIndex].apellido = apellido;
+  usuarios[usuarioIndex].email = email;
+  res.json({
+    usuario: usuarios[usuarioIndex],
+  });
+};
 
 const eliminarUsuario = (req, res) => {};
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -173,4 +173,89 @@ router.get("/:id", usuariosController.getUsuario);
 
 router.post("/", usuariosController.crearUsuario);
 
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   put:
+ *     summary: Actualiza un usuario
+ *     tags: [Usuarios]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - nombre
+ *               - email
+ *             properties:
+ *               nombre:
+ *                 type: string
+ *                 description: Nombre del usuario
+ *               apellido:
+ *                 type: string
+ *                 description: Apellido del usuario
+ *               email:
+ *                 type: string
+ *                 description: Email del
+ *                 format: email
+ *     responses:
+ *       200:
+ *         description: Usuario actualizado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 usuario:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       description: ID del usuario
+ *                     nombre:
+ *                       type: string
+ *                       description: Nombre del usuario
+ *                     apellido:
+ *                       type: string
+ *                       description: Apellido del usuario
+ *                     email:
+ *                       type: string
+ *                       description: Email del usuario
+ *                       format: email
+ *       400:
+ *         description: Error de validación
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 err:
+ *                   type: boolean
+ *                   description: Indica si hubo un error
+ *                 message:
+ *                   type: string
+ *                   description: Mensaje de error
+ *       404:
+ *         description: Usuario no encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 err:
+ *                   type: boolean
+ *                   description: Indica si hubo un error
+ *                 message:
+ *                   type: string
+ *                   description: Mensaje de error
+ */
+router.put("/:id", usuariosController.actualizarUsuario);
+
 export default router;
